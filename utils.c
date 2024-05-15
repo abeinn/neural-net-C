a92266b8320e4a20b6e28a9737d946fee1a8b185
Abein Abraham <aabraham@berkeley.edu>
2024-05-14 17:33:29 -0700

    Updated function names

--- utils.c
+++ utils.c
@@ -8,51 +8,51 @@ typedef struct {
 } vector;
 
 typedef struct {
-    size_t m;
-    size_t n;
+    size_t rows;
+    size_t cols;
     double **data; 
 } matrix;
 
 double rand_weight() { return ((double) rand()) / ((double) RAND_MAX); }
 
-void checkAlloc(void* ptr) {
+void check_alloc(void* ptr) {
     if (ptr == NULL) {
         printf("Error: Memory not allocated\n");
         exit(0);
     }
 }
 
-vector* zeroVector(size_t length) {
+vector* zero_vector(size_t length) {
     vector* vec = malloc(sizeof(vector));
-    checkAlloc(vec);
+    check_alloc(vec);
     vec->length = length; 
     vec->data = calloc(length, sizeof(double));
-    checkAlloc(vec->data);
+    check_alloc(vec->data);
     return vec;
 }
 
-vector* randVector(size_t length) {
-    vector* vec = zeroVector(length);
+vector* rand_vector(size_t length) {
+    vector* vec = zero_vector(length);
     for (int i = 0; i < length; i++) {
         vec->data[i] = rand_weight();
     }
     return vec;
 }
 
-vector* vectorFromArray(double* arr, size_t length) {
-    vector* vec = zeroVector(length);
+vector* vector_from_array(double* arr, size_t length) {
+    vector* vec = zero_vector(length);
     for (int i = 0; i < length; i++) {
         vec->data[i] = arr[i];
     }
     return vec; 
 }
 
-void freeVector(vector* vec) {
+void free_vector(vector* vec) {
     free(vec->data);
     free(vec);
 }
 
-void printVector(vector* vec) {
+void print_vector(vector* vec) {
     size_t length = vec->length;
     double* data = vec->data;
     printf("[");
@@ -62,7 +62,7 @@ void printVector(vector* vec) {
     printf("%g]\n", data[length - 1]);
 }
 
-size_t checkLengths(vector* vec1, vector* vec2) {
+size_t check_lengths(vector* vec1, vector* vec2) {
     size_t length1 = vec1->length;
     size_t length2 = vec2->length;
     if (length1 != length2) {
@@ -72,8 +72,8 @@ size_t checkLengths(vector* vec1, vector* vec2) {
     return length1;
 }
 
-void addToVector(vector* vec1, vector* vec2) {
-    size_t length = checkLengths(vec1, vec2);
+void add_to_vector(vector* vec1, vector* vec2) {
+    size_t length = check_lengths(vec1, vec2);
     double* data1 = vec1->data;
     double* data2 = vec2->data;
     for (int i = 0; i < length; i++) {
@@ -81,18 +81,18 @@ void addToVector(vector* vec1, vector* vec2) {
     }
 }
 
-vector* addVectors(vector* vec1, vector* vec2) {
-    size_t length = checkLengths(vec1, vec2);
+vector* add_vectors(vector* vec1, vector* vec2) {
+    size_t length = check_lengths(vec1, vec2);
     double* data1 = vec1->data;
     double* data2 = vec2->data;
-    vector* vec = zeroVector(length);
+    vector* vec = zero_vector(length);
     for (int i = 0; i < length; i++) {
         vec->data[i] = data1[i] + data2[i];
     }
 }
 
-void copyVector(vector* vec1, vector* vec2) {
-    size_t length = checkLengths(vec1, vec2);
+void copy_vector(vector* vec1, vector* vec2) {
+    size_t length = check_lengths(vec1, vec2);
     double* data1 = vec1->data;
     double* data2 = vec2->data;
     for (int i = 0; i < length; i++) {
@@ -100,8 +100,8 @@ void copyVector(vector* vec1, vector* vec2) {
     }
 }
 
-double dotProduct(vector* vec1, vector* vec2) {
-    size_t length = checkLengths(vec1, vec2);
+double dot_product(vector* vec1, vector* vec2) {
+    size_t length = check_lengths(vec1, vec2);
     double* data1 = vec1->data;
     double* data2 = vec2->data;
     double s = 0.0f;
@@ -111,9 +111,9 @@ double dotProduct(vector* vec1, vector* vec2) {
     return s;
 }
 
-vector* elemProduct(vector* vec1, vector* vec2) {
-    size_t length = checkLengths(vec1, vec2);
-    vector* vec = zeroVector(length);
+vector* elem_product(vector* vec1, vector* vec2) {
+    size_t length = check_lengths(vec1, vec2);
+    vector* vec = zero_vector(length);
     double* data1 = vec1->data;
     double* data2 = vec2->data;
     double* data = vec->data;
@@ -123,21 +123,37 @@ vector* elemProduct(vector* vec1, vector* vec2) {
     return vec;
 }
 
-matrix* zeroMatrix(size_t length) {
-    vector* vec = malloc(sizeof(vector));
-    checkAlloc(vec);
-    vec->length = length; 
-    vec->data = calloc(length, sizeof(double));
-    checkAlloc(vec->data);
-    return vec;
+matrix* zero_matrix(size_t rows, size_t cols) {
+    matrix* mat = malloc(sizeof(matrix));
+    check_alloc(mat);
+    mat->rows = rows;
+    mat->cols = cols; 
+    double** data = malloc(rows, sizeof(double*));
+    check_alloc(data);
+    mat->data = data;
+    for (int i = 0; i < rows; i++) {
+        data[i] = calloc(cols, sizeof(double));
+        check_alloc(data[i]);
+    }
+    return mat;
+}
+
+matrix* rand_matrix(size_t rows, size_t cols) {
+    matrix* mat = zero_matrix(rows, cols);
+    double** data = mat->data;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            data[i][j] = rand_weight();
+        }
+    }
 }
 
 int main(void) {
     double arr1[] = {1, 2, 3};
     double arr2[] = {-1, 4, 2};
-    vector* vec1 = vectorFromArray(arr1, 3);
-    vector* vec2 = vectorFromArray(arr2, 3);
-    printf("%g\n", dotProduct(vec1, vec2));
-    vector* vec3 = elemProduct(vec1, vec2);
-    printVector(vec3);
+    vector* vec1 = vector_from_array(arr1, 3);
+    vector* vec2 = vector_from_array(arr2, 3);
+    printf("%g\n", dot_product(vec1, vec2));
+    vector* vec3 = elem_product(vec1, vec2);
+    print_vector(vec3);
 }
